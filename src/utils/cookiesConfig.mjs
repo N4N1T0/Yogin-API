19856d47e4666cbbed59e0cf65a680043f6dfada
Yogin-2425 <proyectoyogin.vs@gmail.com>
2024-10-08 23:07:11 +0200

    update: middleware to setCookies

---
+++ src/utils/cookiesConfig.mjs
@@ -0,0 +1,24 @@
+// cookieConfig.mjs
+
+export const cookiesConfig = {
+  secure: process.env.NODE_ENV === "production",
+  httpOnly: true,
+  maxAge: 60 * 60 * 1000, // 1 hora
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  signed: true,
+  domain: process.env.NODE_ENV === "production" ? ".yog-in.es" : "localhost",
+};
+
+export const setCookies = (req, res) => {
+  res.cookie(
+    "sessionData",
+    JSON.stringify({
+      userId: req.session.userId,
+      role: req.session.role,
+      initialRole: req.session.initialRole,
+    }),
+    cookiesConfig // Usar la configuración de cookies
+  );
+
+  res.cookie("sessionId", req.session.id, cookiesConfig); // También usa cookieConfig aquí
+};
---
+++ src/utils/index.mjs
@@ -0,0 +1,4 @@
+export { users } from "./constants.mjs";
+export { createUserValidationSchema } from "./validationSchema.mjs";
+export { resolveIndexById } from "./middlewares.mjs";
+export { cookiesConfig, setCookies } from "./cookiesConfig.mjs";
