5570f83c983aada0f47b0b5eb74b117f23670a85
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2024-12-16 17:22:42 +0100

    feat: Add auth utils for the JWT

---
+++ src/utils/auth-utils.mjs
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/index.js";
+
+export const generateJWT = (user) => {
+  const payload = {
+    userId: user._id,
+    role: user.role,
+    initialRole: user.role,
+  }; // Puedes agregar más datos al payload
+  const token = jwt.sign(payload, "your_jwt_secret", { expiresIn: "72h" }); // Ajusta el tiempo de expiración según necesites
+  return token;
+};
+
+// Función para obtener el usuario desde el payload del JWT
+export const getUserFromJwt = async (jwt_payload) => {
+  try {
+    // Obtener el usuario desde la base de datos usando el ID del payload
+    const user = await User.findById(jwt_payload.userId);
+
+    if (!user) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    return user; // Retorna el usuario encontrado
+  } catch (error) {
+    throw new Error("Error al verificar el token JWT", error);
+  }
+};
