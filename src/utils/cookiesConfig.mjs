b218e733dbe0768c37450915c2d514a36e505f3b
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2024-12-16 17:17:47 +0100

    feat: Change the cookieConfig to integrate with passport-jwt

--- src/utils/cookiesConfig.mjs
+++ src/utils/cookiesConfig.mjs
@@ -1,28 +1,17 @@
-// cookieConfig.mjs
-
 export const cookiesConfig = {
   secure: process.env.NODE_ENV === "production",
   httpOnly: true,
-  maxAge: 60 * 60 * 1000, // 1 hora
+  maxAge: 72 * 60 * 60 * 1000, // 72 hora
   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-  signed: true,
+  // signed: true, ---> no se necesita con los JWT, ya vienen encriptados
   // domain: process.env.NODE_ENV === "production" ? ".yog-in.es" : "localhost",
   domain:
     process.env.NODE_ENV === "production"
       ? "yogin-api-lilac.vercel.app"
-      : "localhost",
+      : undefined,
 };
 
-export const setCookies = (req, res) => {
-  res.cookie(
-    "sessionData",
-    JSON.stringify({
-      userId: req.session?.userId,
-      role: req.session?.role,
-      initialRole: req.session?.initialRole,
-    }),
-    cookiesConfig // Usar la configuración de cookies
-  );
-
-  res.cookie("sessionId", req.session.id, cookiesConfig); // También usa cookieConfig aquí
+export const setCookies = (_req, res, token) => {
+  // Establecer el token JWT en una cookie
+  res.cookie("sessionToken", token, cookiesConfig);
 };
